32793fa5134ad9b85fa4baa670a34b523a86b819
UTKARSH DANDAGAVHAL <81460601+UtkarshDandagavhal@users.noreply.github.com>
2023-06-06 17:09:30 +0530

    Update class1.cpp

--- class1.cpp
+++ class1.cpp
@@ -8,6 +8,29 @@ public:
   int age;
   string name;
   
+  // construtor by default called now - also know default construtor
+  Animal(){
+      this->weight = 0;
+      this->age = 0;
+      this->name = "";
+  }
+  Animal(int weight){
+      this->weight = weight;
+      cout<<"parametrised construtor1 called"<<endl;
+  }
+  // parametrised construtor
+  Animal(int weight, int age){
+      this->weight = weight;
+      this->age = age;
+      cout<<"parametrised construtor2 called";
+  }
+  
+  Animal(int weight, int age, string name){
+      this->weight = weight;
+      this->age = age;
+      this->name = name;
+      cout<<"parametrised construtor3 called"<<endl;
+  }
   // function create
   void eat(){
       cout<<"Eat now"<<endl;
@@ -24,24 +47,61 @@ public:
   void settweight(int w){
       weight = w;
   }
+  
+  //copy construtor
+  Animal(Animal &obj){
+      this->age = obj.age;
+      this->weight = obj.weight;
+      this->name = obj.name;
+      cout<<"its copy construtor"<<endl;
+  }
+  
 };
 int main(){
     // OBJECT CREATION
-    // dynamic
     // static
-    Animal ramesh;
-    ramesh.age = 12;
-    ramesh.name = "lion";
+    // Animal ramesh;
+    // ramesh.age = 12;
+    // ramesh.name = "lion";
     
-    cout<<"Age of ramesh: "<<ramesh.age<<endl;
-    cout<<"name of ramesh: "<<ramesh.name<<endl;
+    // cout<<"Age of ramesh: "<<ramesh.age<<endl;
+    // cout<<"name of ramesh: "<<ramesh.name<<endl;
     // cout<<"weight of ramesh: "<<ramesh.weight<<endl;
     
-    ramesh.settweight(101);
-    cout<<"weight of ramesh: "<<ramesh.gettweight()<<endl;
+    // ramesh.settweight(101);
+    // cout<<"weight of ramesh: "<<ramesh.gettweight()<<endl;
     
-    ramesh.eat();
-    ramesh.sleep();
+    // ramesh.eat();
+    // ramesh.sleep();
     
+    // dynamic memory allocation 
+    // create an OBJECT
+    // Animal* Suresh = new Animal;
+    
+    // access the elements 
+    // (*Suresh).age = 15;
+    // (*Suresh).name = "bill";
+    
+    
+    // (*Suresh).eat();
+    // (*Suresh).sleep();
+    
+    // alternate way to access element using arrow operator
+    // Suresh->age = 17;
+    // Suresh->name = "ok";
+    
+    // Suresh->eat();
+    // Suresh->sleep();
+    
+    
+    Animal a(10);
+    Animal* b = new Animal(50,10,"xuv");
+    
+    
+    // Object copy
+    Animal c = a;
+    
+    //another way to copy Object
+    Animal animal1(c);
     
 }
