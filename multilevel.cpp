580c9aee34620517c7c675d8a87f8e8d339c8deb
UTKARSH DANDAGAVHAL <81460601+UtkarshDandagavhal@users.noreply.github.com>
2023-06-06 23:44:14 +0530

    Create multilevel.cpp

---
+++ multilevel.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class Fruit{
+    public:
+        int price;
+};
+class mango: public Fruit{
+    
+};
+class Amp: mango{
+    
+};
+int main(){
+    Amp a;
+    
+}
